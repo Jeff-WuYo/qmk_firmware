c0c1c7aa05ef0a22599d68c8d8e0b94bd4be1251
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2024-04-07 17:39:47 +0800

    add colemak-dh modified layout

--- keyboards/phoenix/keymaps/via/keymap.c
+++ keyboards/phoenix/keymaps/via/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | MUTE |       | HOME |        |      |
  *                                 | Space| BSPC |------|       |------| Enter  | Space|
- *                                 |      |      | MO(1)|       | MO(1)|        |      |
+ *                                 |      |      | TG(2)|       | MO(1)|        |      |
  *                                 `--------------------'       `----------------------'
  */
 [BASE] = LAYOUT_ergodox(
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MO(1),   KC_DEL,  KC_LALT, KC_LBRC, KC_RBRC,
                                                           KC_VOLD, KC_VOLU,
                                                                    KC_MUTE,
-                                                 KC_SPC,  KC_BSPC, MO(1),
+                                                 KC_SPC,  KC_BSPC, TG(2),
 
              KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
              KC_RCTL, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   `    |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |  F12 |  F6  |  F7  |  F8  |  F9  | F10  |  F11   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        | BTN1 | MS_U | BTN2 | BTN3 | TMgr |      |           |      |      |      |Insert|      | PSCR |        |
+ * |        | BTN1 | MS_U | BTN2 | BTN3 | TMgr |      |           |      |      | Pause|Insert|      | PSCR |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |  Caps  | MS_L | MS_D | MS_R |      |      |------|           |------| LEFT | DOWN |  UP  |RIGHT |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  KC_ENT,  KC_TRNS, KC_TRNS,
 
              KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,  KC_TRNS, KC_PSCR, KC_TRNS,
+             KC_TRNS, KC_TRNS, KC_PAUS, KC_INS,  KC_TRNS, KC_PSCR, KC_TRNS,
                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,
              KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_MPLY, KC_TRNS,
                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -90,40 +90,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 2: Other layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |  ESC`  |   1  |   2  |   3  |   4  |   5  |   6  |           |  EQL |   6  |   7  |   8  |   9  |   0  |  MINS  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |  TAB   |   Q  |   W  |   F  |   P  |   B  |  F5  |           | RCTL |   K  |   H  |   U  |   Y  |  ;   |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |------|           |------|      |      |      |      |      |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |  LCTL  |   A  |   R  |   S  |   T  |   G  |------|           |------|   M  |   N  |   E  |   I  |  O   |   '    |
+ * |--------+------+------+------+------+------| LGUI |           | RGUI |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   D  |   V  |      |           |      |   J  |   L  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   | MO(1)|Delete| LALT |   [  |   ]  |                                       | LEFT | DOWN |  UP  | RIGHT| MO(1)  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        |      |      |       |      |        |
+ *                                        | VOLD | VOLU |       | PGDN |  PGUP  |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      |      |       |      |        |      |
- *                                 |      |      |------|       |------|        |      |
- *                                 |      |      |      |       |      |        |      |
+ *                                 |      |      | MUTE |       | HOME |        |      |
+ *                                 | Space| BSPC |------|       |------| Enter  | Space|
+ *                                 |      |      | TG(2)|       | MO(1)|        |      |
  *                                 `--------------------'       `----------------------'
  */
 [SECOND] = LAYOUT_ergodox(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                                          KC_TRNS, KC_TRNS,
-                                                                   KC_TRNS,
-                                                 KC_TRNS, KC_TRNS, KC_TRNS,
+    QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
+    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_F5,
+    KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_LGUI,
+    MO(1),   KC_DEL,  KC_LALT, KC_LBRC, KC_RBRC,
+                                                          KC_VOLD, KC_VOLU,
+                                                                   KC_MUTE,
+                                                 KC_SPC,  KC_BSPC, TG(2),
 
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS,
-    KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS),
+             KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+             KC_RCTL, KC_K,    KC_H,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
+                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+             KC_RGUI, KC_J,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, MO(1),
+    KC_PGDN, KC_PGUP,
+    KC_HOME,
+    MO(1),   KC_ENT,  KC_SPC),
 };
