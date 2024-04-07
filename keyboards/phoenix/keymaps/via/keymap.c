262d1c788d75c7297c0e8a8bba169cde23e31bbb
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2024-04-07 19:19:35 +0800

    change layout order

--- keyboards/phoenix/keymaps/via/keymap.c
+++ keyboards/phoenix/keymaps/via/keymap.c
@@ -1,9 +1,9 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-    BASE,  // default layer
-    FIRST, // functions
-    SECOND, // other
+    BASE,  // default qwerty
+    FIRST, // colemak-dh modified
+    SECOND, // functinos
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -18,14 +18,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| LGUI |           | RGUI |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | MO(1)|Delete| LALT |   [  |   ]  |                                       | LEFT | DOWN |  UP  | RIGHT| MO(1)  |
+ *   | MO(2)|Delete| LALT |   [  |   ]  |                                       | LEFT | DOWN |  UP  | RIGHT| MO(2)  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | VOLD | VOLU |       | PGDN |  PGUP  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | MUTE |       | HOME |        |      |
  *                                 | Space| BSPC |------|       |------| Enter  | Space|
- *                                 |      |      | TG(2)|       | MO(1)|        |      |
+ *                                 |      |      | TG(1)|       | MO(2)|        |      |
  *                                 `--------------------'       `----------------------'
  */
 [BASE] = LAYOUT_ergodox(
@@ -33,21 +33,61 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_F5,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,
-    MO(1),   KC_DEL,  KC_LALT, KC_LBRC, KC_RBRC,
+    MO(2),   KC_DEL,  KC_LALT, KC_LBRC, KC_RBRC,
                                                           KC_VOLD, KC_VOLU,
                                                                    KC_MUTE,
-                                                 KC_SPC,  KC_BSPC, TG(2),
+                                                 KC_SPC,  KC_BSPC, TG(1),
 
              KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
              KC_RCTL, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
              KC_RGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, MO(1),
+                               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, MO(2),
     KC_PGDN, KC_PGUP,
     KC_HOME,
-    MO(1),   KC_ENT,  KC_SPC),
+    MO(2),   KC_ENT,  KC_SPC),
 
-/* Keymap 1: Function layer
+/* Keymap 1: colemak-dk modified
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |  ESC`  |   1  |   2  |   3  |   4  |   5  |   6  |           |  EQL |   6  |   7  |   8  |   9  |   0  |  MINS  |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |  TAB   |   Q  |   W  |   F  |   P  |   B  |  F5  |           | RCTL |   K  |   H  |   U  |   Y  |  ;   |   \    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |  LCTL  |   A  |   R  |   S  |   T  |   G  |------|           |------|   M  |   N  |   E  |   I  |  O   |   '    |
+ * |--------+------+------+------+------+------| LGUI |           | RGUI |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   D  |   V  |      |           |      |   J  |   L  |   ,  |   .  |  /   | RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | MO(2)|Delete| LALT |   [  |   ]  |                                       | LEFT | DOWN |  UP  | RIGHT| MO(2)  |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,---------------.
+ *                                        | VOLD | VOLU |       | PGDN |  PGUP  |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      |      | MUTE |       | HOME |        |      |
+ *                                 | Space| BSPC |------|       |------| Enter  | Space|
+ *                                 |      |      | TG(1)|       | MO(2)|        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+[FIRST] = LAYOUT_ergodox(
+    QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
+    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_F5,
+    KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_LGUI,
+    MO(2),   KC_DEL,  KC_LALT, KC_LBRC, KC_RBRC,
+                                                          KC_VOLD, KC_VOLU,
+                                                                   KC_MUTE,
+                                                 KC_SPC,  KC_BSPC, TG(1),
+
+             KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+             KC_RCTL, KC_K,    KC_H,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
+                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
+             KC_RGUI, KC_J,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, MO(2),
+    KC_PGDN, KC_PGUP,
+    KC_HOME,
+    MO(2),   KC_ENT,  KC_SPC),
+
+/* Keymap 2: Function layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   `    |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |  F12 |  F6  |  F7  |  F8  |  F9  | F10  |  F11   |
@@ -68,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
-[FIRST] = LAYOUT_ergodox(
+[SECOND] = LAYOUT_ergodox(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
     KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3, RCS(KC_ESC), KC_TRNS,
     KC_CAPS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS,
@@ -86,44 +126,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS,
     KC_END,
     KC_TRNS, KC_TRNS, KC_BSPC),
-
-/* Keymap 2: Other layer
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  ESC`  |   1  |   2  |   3  |   4  |   5  |   6  |           |  EQL |   6  |   7  |   8  |   9  |   0  |  MINS  |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  TAB   |   Q  |   W  |   F  |   P  |   B  |  F5  |           | RCTL |   K  |   H  |   U  |   Y  |  ;   |   \    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  LCTL  |   A  |   R  |   S  |   T  |   G  |------|           |------|   M  |   N  |   E  |   I  |  O   |   '    |
- * |--------+------+------+------+------+------| LGUI |           | RGUI |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   D  |   V  |      |           |      |   J  |   L  |   ,  |   .  |  /   | RShift |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | MO(1)|Delete| LALT |   [  |   ]  |                                       | LEFT | DOWN |  UP  | RIGHT| MO(1)  |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,---------------.
- *                                        | VOLD | VOLU |       | PGDN |  PGUP  |
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | MUTE |       | HOME |        |      |
- *                                 | Space| BSPC |------|       |------| Enter  | Space|
- *                                 |      |      | TG(2)|       | MO(1)|        |      |
- *                                 `--------------------'       `----------------------'
- */
-[SECOND] = LAYOUT_ergodox(
-    QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,
-    KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_F5,
-    KC_LCTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_LGUI,
-    MO(1),   KC_DEL,  KC_LALT, KC_LBRC, KC_RBRC,
-                                                          KC_VOLD, KC_VOLU,
-                                                                   KC_MUTE,
-                                                 KC_SPC,  KC_BSPC, TG(2),
-
-             KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-             KC_RCTL, KC_K,    KC_H,    KC_U,    KC_Y,    KC_SCLN, KC_BSLS,
-                      KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,
-             KC_RGUI, KC_J,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                               KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, MO(1),
-    KC_PGDN, KC_PGUP,
-    KC_HOME,
-    MO(1),   KC_ENT,  KC_SPC),
 };
