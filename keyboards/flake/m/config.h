242480cf8613f2c7fd57856c440788df1330abf9
Jeff-WuYo <62562200+Jeff-WuYo@users.noreply.github.com>
2025-08-17 22:41:54 +0800

    Initial commit of flake/m.
    This contain basic keyboard.json and default keymap. readme.md is no done.

---
+++ keyboards/flake/m/config.h
@@ -0,0 +1,25 @@
+/* Copyright 2019
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#define EE_HANDS
+#define SPLIT_MAX_CONNECTION_ERRORS 10
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17 // Specify a optional status led by GPIO number which blinks when entering the bootloader
+//#define SERIAL_USART_TX_PIN GP1
+//#define SPLIT_USB_TIMEOUT 2000
+//#define SPLIT_USB_TIMEOUT_POLL 10
+//#define SPLIT_WATCHDOG_ENABLE
+//#define SPLIT_WATCHDOG_TIMEOUT 3000
